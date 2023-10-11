ccf09757f0eaa63fb9e808565a0b3f844825ddc3
Richard Stephen Barboza Alarcón <rbarboza@tredasolutions.com>
2023-10-10 21:23:43 -0500

    Agrega endpoint para mostrar productos

--- src/Componentes/Card.js
+++ src/Componentes/Card.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../EstilosComponentes/Card.css'
 import imagenCards from "../Imagenes/reloj1.jpg";
 import CarritoCompras from './CarritoCompras';
@@ -15,65 +15,46 @@ function Card({ productName, price, imageUrl, descripcion, agregarProducto }) {
   const primerProducto = productos[0];
   const segundoProducto = productos[1];
   const tercerProducto = productos[2];
+  const [data, setData] = useState([]);
+  function getProducts() {
+    const response =  fetch("https://fakestoreapi.com/products", {
+      method: 'GET',
+      headers: {'Content-Type': 'application/json'}
+    }).then(res=>res.json())
+        .then(json=>setData(json))
+        .catch((error) => {
+          //Error
+          alert(JSON.stringify(error));
+          console.error(error);
+        });
+  }
+  useEffect(()=>{
+    getProducts()
+  },[]);
 
   return (
     <>
-    <div className="shopping-card-container" >
-      {/* Renderiza solo el primer producto */}
-      <div >
-        <div className="shopping-card">
-          <div key={primerProducto.id} className="product-info">
-            <img src={primerProducto.imagen} alt={productName} className="product-image" />
-            <h3 className="product-name">{primerProducto.nombre}</h3>
-            {/* <p className="descripcion">{descripcion}</p> */}
-            <p className="product-price">${primerProducto.precio}</p>
-            <button
-              className="btn success add-to-cart-button"
-              onClick={() => {agregarProducto(primerProducto)}}
-            >
-              Agregar al carrito
-            </button>
-          </div>
+      <div className="">
+      {data.map((element)=>(
+        <div className="shopping-card-container" >
+            <div className="shopping-card">
+                  <div key={element.id} className="product-info">
+                    <img src={element.image} alt={productName} className="product-image" />
+                    <h3 className="product-name">{element.title}</h3>
+                    {/* <p className="descripcion">{descripcion}</p> */}
+                    <p className="product-price">${element.price}</p>
+                    <button
+                        className="btn success add-to-cart-button"
+                        onClick={() => {agregarProducto(primerProducto)}}
+                    >
+                      Agregar al carrito
+                    </button>
+                  </div>
+            </div>
         </div>
+      ))}
       </div>
-
-      {/* Renderiza solo el segundo producto */}
-      <div className="shopping-card-container">
-        <div className="shopping-card">
-          <div key={segundoProducto.id} className="product-info">
-            <img src={imagenCards} alt={productName} className="product-image" />
-            <h3 className="product-name">{segundoProducto.nombre}</h3>
-            {/* <p className="descripcion">{descripcion}</p> */}
-            <p className="product-price">${segundoProducto.precio}</p>
-            <button
-              className="btn success add-to-cart-button"
-              onClick={() => agregarProducto(segundoProducto)}
-            >
-              Agregar al carrito
-            </button>
-          </div>
-        </div>
-      </div>
-
-       {/* Renderiza solo el tercer producto */}
-       <div className="shopping-card-container">
-        <div className="shopping-card">
-          <div key={tercerProducto.id} className="product-info">
-            <img src={imagenCards} alt={productName} className="product-image" />
-            <h3 className="product-name">{tercerProducto.nombre}</h3>
-            {/* <p className="descripcion">{descripcion}</p> */}
-            <p className="product-price">${tercerProducto.precio}</p>
-            <button
-              className="btn success add-to-cart-button"
-              onClick={() => agregarProducto(tercerProducto)}
-            >
-              Agregar al carrito
-            </button>
-          </div>
-        </div>
-      </div>
-      </div>
-    </>
+      </>
   );
 }
 
